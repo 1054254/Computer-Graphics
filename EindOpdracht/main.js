630cdbd93028ddaedb31c803c8e7d4a76f3a6334
1054254 <1054254@hr.nl>
2025-12-01 11:20:52 +0100

    same as last commit

--- EindOpdracht/main.js
+++ EindOpdracht/main.js
@@ -324,8 +324,7 @@
     const f = 1.0 / Math.tan(fieldOfViewInRadians / 2);
     const rangeInv = 1 / (near - far);
 
-    // prettier-ignore
-        return [
+    return [
             f / aspectRatio, 0, 0, 0,
             0, f, 0, 0,
             0, 0, (near + far) * rangeInv, -1,
